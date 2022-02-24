8ef6dfb38c71df633dadb6e3c8c3c78b8e5bdea0
Eduardo Beyra <eduardobeyra@hotmail.com>
2022-02-24 14:29:10 -0500

    iteration 4 eddie

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@
 import React from 'react';
 import './App.css';
 import foodsArr from './foods.json';
-import FoodBox from './components/FoodBox'
+import FoodBox from './components/FoodBox';
+import AddFoodForm from './components/AddFoodForm';
 
 function App() {
   const [foods, setFoods] = React.useState(foodsArr);
 
-  
   return (
     <div className="App">
+      <AddFoodForm foodsArr={foods} setFoodsArr={setFoods} />
       {foods.map((food) => {
-        return (
-          <FoodBox foods={food} />
-        );
+        return <FoodBox foods={food} />;
       })}
     </div>
   );
---
+++ src/components/AddFoodForm.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Divider, Input } from 'antd';
+
+function AddFoodForm(props) {
+  const [name, setName] = React.useState('');
+  const [image, setImage] = React.useState('');
+  const [calories, setCalories] = React.useState('');
+  const [servings, setServings] = React.useState('');
+
+  const addFood = (e) => {
+    e.preventDefault();
+    let foodArrayCopy = [...props.foodsArr];
+    foodArrayCopy.unshift({
+      name: name,
+      image: image,
+      calories: calories,
+      servings: servings,
+    });
+    props.setFoodsArr(foodArrayCopy);
+    // props.setSearchFood(foodArrayCopy);
+  };
+
+  return (
+    <form onSubmit={addFood}>
+      <Divider>Add Food Entry</Divider>
+
+      <label>Name</label>
+      <Input
+        value={name}
+        type="text"
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <label>Image</label>
+      <Input
+        value={image}
+        type="text"
+        onChange={(e) => setImage(e.target.value)}
+      />
+
+      <label>Calories</label>
+      <Input
+        value={calories}
+        type="text"
+        onChange={(e) => setCalories(e.target.value)}
+      />
+
+      <label>Servings</label>
+      <Input
+        value={servings}
+        type="text"
+        onChange={(e) => setServings(e.target.value)}
+      />
+
+      <button type="submit">Create</button>
+    </form>
+  );
+}
+
+export default AddFoodForm;
